725cb7fb0c11bae23c23723d35b87744cdd5f51c
Lucas <lucas@Lucas-Macbook.local>
2024-08-20 00:51:58 +0200

    Configured app to use Heroku PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from app.models import db  # Import the db object from models.py
+from config import Config  # Import the Config class from config.py
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    
+    # Load configuration from Config class
+    app.config.from_object(Config)
 
-    db.init_app(app)  # Initialize the database with the Flask app
+    # Initialize the database with the Flask app
+    db.init_app(app)
 
+    # Ensure that the tables are created within the app context
     with app.app_context():
         db.create_all()  # Create all database tables
-
+    
+    # Import and register the pages blueprint
     from .pages import bp as pages_bp
     app.register_blueprint(pages_bp)
 
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
